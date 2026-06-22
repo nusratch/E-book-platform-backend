86c51e6d865d6c08c33441a8f225fdc94ba087dc
nusrat <nusratchy.002@gmail.com>
2026-06-22 18:56:51 +0600

    Connect MongoDB to Express server

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcz5eav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +23,16 @@ app.get("/", (req, res) => {
   res.send("Ebook Platform Server Running");
 });
 
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("MongoDB Connected Successfully");
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
