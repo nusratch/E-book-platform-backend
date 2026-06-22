c629e7bf54db8b025ea929cd25ebe6c525f15fcd
nusrat <nusratchy.002@gmail.com>
2026-06-22 19:02:37 +0600

    Create ebooks API endpoint

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcz5eav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -27,12 +28,24 @@ async function run() {
   try {
     await client.connect();
 
+    const database = client.db("ebookPlatformDB");
+    const ebooksCollection = database.collection("ebooks");
+
     console.log("MongoDB Connected Successfully");
-  } finally {
+
+    // Get all ebooks
+    app.get("/ebooks", async (req, res) => {
+      const result = await ebooksCollection.find().toArray();
+      res.send(result);
+    });
+
+  } catch (error) {
+    console.log(error);
   }
 }
 
-run().catch(console.dir);
+run();
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
