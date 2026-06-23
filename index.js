1b9721102f3cd46cfb393d97ddec293dd5ac1f0b
nusrat <nusratchy.002@gmail.com>
2026-06-23 18:05:47 +0600

    add users and jwt routes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
 
 const app = express();
@@ -28,19 +30,105 @@ async function run() {
   try {
     await client.connect();
 
+    const database = client.db("ebookPlatform");
 
-   const database = client.db("ebookPlatform");
-const ebooksCollection = database.collection("ebooks");
-  
+    const ebooksCollection = database.collection("ebooks");
+    const usersCollection = database.collection("users");
 
     console.log("MongoDB Connected Successfully");
 
-  
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(
+        user,
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "7d",
+        }
+      );
+
+      res.send({ token });
+    });
+
+    app.post("/users", async (req, res) => {
+      try {
+        const user = req.body;
+
+        const existingUser = await usersCollection.findOne({
+          email: user.email,
+        });
+
+        if (existingUser) {
+          return res.status(400).send({
+            message: "User already exists",
+          });
+        }
+
+        const result = await usersCollection.insertOne(user);
+
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+
+        res.status(500).send({
+          message: "Server Error",
+        });
+      }
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+
+        const result = await usersCollection.findOne({
+          email,
+        });
+
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+
+        res.status(500).send({
+          message: "Server Error",
+        });
+      }
+    });
+
     app.get("/ebooks", async (req, res) => {
       const result = await ebooksCollection.find().toArray();
       res.send(result);
     });
 
+    app.get("/ebooks/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({
+            message: "Invalid ebook id",
+          });
+        }
+
+        const result = await ebooksCollection.findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!result) {
+          return res.status(404).send({
+            message: "Ebook not found",
+          });
+        }
+
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+
+        res.status(500).send({
+          message: "Server Error",
+        });
+      }
+    });
   } catch (error) {
     console.log(error);
   }
