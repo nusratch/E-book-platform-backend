7e33495dd2a98fbd6016b33318b1d971ad7768d9
nusrat <nusratchy.002@gmail.com>
2026-07-02 22:24:03 +0600

    delete duplicate

--- index.js
+++ index.js
@@ -331,26 +331,6 @@ app.post("/bookmarks", async (req, res) => {
       }
     });
 
-    app.get("/writer/:email", async (req, res) => {
-      try {
-        const result = await ebooksCollection
-          .find({
-            writerEmail: req.params.email,
-          })
-          .sort({
-            createdAt: -1,
-          })
-          .toArray();
-
-        res.send(result);
-      } catch (error) {
-        console.log(error);
-
-        res.status(500).send({
-          message: "Server Error",
-        });
-      }
-    });
 
     app.get("/ebooks/:id", async (req, res) => {
       try {
