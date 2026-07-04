8b6c73aae8f786af4228735117c3ca297d4fe8a2
nusrat <nusratchy.002@gmail.com>
2026-07-04 20:40:21 +0600

    Simplify MongoDB connection

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { auth } from "./auth.js";
+//import { auth } from "./auth.js";
 import { toNodeHandler } from "better-auth/node";
 import { MongoClient, ObjectId } from "mongodb";
 import express from "express";
@@ -20,7 +20,7 @@ app.use(
 );
 
 app.use(express.json());
-app.all("/api/auth/*", toNodeHandler(auth));
+//app.all("/api/auth/*", toNodeHandler(auth));
 
 const uri = process.env.MONGO_URI;
 
